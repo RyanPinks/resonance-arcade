e086c549e05ef5447dfe41b57d3492568b20f9c4
RyanPinks <ryanpinks64@gmail.com>
2025-10-25 09:51:42 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let games = {}; // key: roomId, value: board state
+
+io.on('connection', (socket) => {
+  socket.on('join', (roomId) => {
+    socket.join(roomId);
+    if (!games[roomId]) {
+      games[roomId] = Array(9).fill(null);
+    }
+    socket.emit('init', games[roomId]);
+  });
+
+  socket.on('move', ({ roomId, index, player }) => {
+    if (!games[roomId] || games[roomId][index]) return;
+    games[roomId][index] = player;
+    io.to(roomId).emit('update', { index, player });
+  });
+
+  socket.on('chat', (msg) => {
+    io.emit('chat', msg);
+  });
+});
+
+server.listen(3000, () => console.log('Resonance Arcade server running on port 3000'));
